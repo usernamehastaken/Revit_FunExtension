897fe44f64b97ab7f11cdb138796142c7aa7af30
cxx <cxx_pb@126.com>
2021-04-27 17:46:26 +0800

    2021-4-27

--- Revit_FunExtension.cs
+++ Revit_FunExtension.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
 
 namespace Revit_FunExtension
 {
@@ -22,11 +23,44 @@ namespace Revit_FunExtension
             transaction.Commit();
         }
         
-        public void n()
+        public static void SetParametersToElement(ElementId elementId)
         {
-            int init1 = 0;
-            int int2 = 1;
             
         }
+
+        public static bool SetNewParameterToInstanceWall(UIApplication app, DefinitionFile myDefinitionFile)
+        {
+            // create a new group in the shared parameters file
+            DefinitionGroups myGroups = myDefinitionFile.Groups;
+            DefinitionGroup myGroup = myGroups.Create("MyParameters1");
+
+            // create an instance definition in definition group MyParameters
+            ExternalDefinitionCreationOptions option = new ExternalDefinitionCreationOptions("Instance_ProductDate", ParameterType.Text);
+            // Don't let the user modify the value, only the API
+            option.UserModifiable = false;
+            // Set tooltip
+            option.Description = "Wall product date";
+            Definition myDefinition_ProductDate = myGroup.Definitions.Create(option);
+
+            // create a category set and insert category of wall to it
+            CategorySet myCategories = app.Application.Create.NewCategorySet();
+            // use BuiltInCategory to get category of wall
+            Category myCategory = Category.GetCategory(app.ActiveUIDocument.Document, BuiltInCategory.OST_Walls);
+
+
+            myCategories.Insert(myCategory);
+
+            //Create an instance of InstanceBinding
+            InstanceBinding instanceBinding = app.Application.Create.NewInstanceBinding(myCategories);
+
+            // Get the BingdingMap of current document.
+            BindingMap bindingMap = app.ActiveUIDocument.Document.ParameterBindings;
+
+            // Bind the definitions to the document
+            bool instanceBindOK = bindingMap.Insert(myDefinition_ProductDate,
+                                            instanceBinding, BuiltInParameterGroup.PG_TEXT);
+            return instanceBindOK;
+        }
+
     }
 }
